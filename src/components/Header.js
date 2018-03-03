585ccec516e09437d0edd1a1dd7e33a41471a092
obodrov <bodrovphone@gmail.com>
2018-03-03 18:50:37 +0200

    #17 added logo and favicon + update the header with them

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,12 @@
 import React from 'react';
 
 // :design assets:
-import logo from '../logo.svg';
+import logo from '../money-watcher_transparent_bg.png';
 
 const Header = () => {
     return (
         <header className="App-header">
-            <img src={ logo } className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <img src={ logo } className="" alt="logo" />
         </header>
         );
 };
