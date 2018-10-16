e21c6ea6725c536fe4e2b28b7f13e14a85aff0cd
Oleksandr Bodrov <obod@softserveinc.com>
2018-10-16 16:43:46 +0300

    #53 updating balances when adding new transaction

--- src/reducers/transactions.js
+++ src/reducers/transactions.js
@@ -13,8 +13,38 @@ import { startPoint as currentMonth } from '../components/Display/currentMonth';
 
 function updateFirebse({payload}) {
     if (!payload.editing) {
+        // adding transaction
         trsRef.child(payload.dateToken).set(payload);
+        // updating relevant balance
         balanceRef.child(payload.dateToken.substring(0, 7)).set(payload.currentBalance + payload.sum);
+        // how many future balances should I update? - All of them
+        // how can I identify all references to future balances => here it is:
+        async function getBalances() {
+            let promise = new Promise((resolve, reject) => {
+                balanceRef.once('value', function(snapshot) {
+                    resolve(snapshot.val())
+                })
+            });
+            let result = await promise;
+            return result;
+        } 
+
+        // I need another function to update all current + future balances
+        function updateBalances(balances) {
+            // but how do I know which balances are future ones?
+            var updatedBalances = {...balances};
+            console.log('current balance', balances);
+            
+            for(let item in balances) {
+                if ((+item.substring(0, 4) >= +payload.dateToken.substring(0, 4)) && (+item.substring(5, 7) >= +payload.dateToken.substring(5, 7))) {
+                    updatedBalances[item] = balances[item] + payload.sum;
+                }
+            }
+            console.log('updated balance', updatedBalances);
+            
+            balanceRef.set(updatedBalances);
+        }
+        getBalances().then(updateBalances);        
     } else {
         delete payload.editing;
         trsRef.child(payload.editedNodeKey).set(null);
